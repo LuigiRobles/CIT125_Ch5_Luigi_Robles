5d5694a49913755811bf8f9a388b442eaa029b11
Luigi.Robles <Luigi.robles0846@my.riohondo.edu>
2020-06-22 22:53:57 -0700

    Update CIT125_Ch5_Luigi_Robles.cpp
    adding comments to completed lab code

--- CIT125_Ch5_Luigi_Robles/CIT125_Ch5_Luigi_Robles.cpp
+++ CIT125_Ch5_Luigi_Robles/CIT125_Ch5_Luigi_Robles.cpp
@@ -2,31 +2,35 @@
 // CIT125 Luigi Robles
 // Ch.5 Pg. 137 Lab 5-1
 // 6-22-2020 Summer Term
-#include <iostream>
-#include <iomanip>
+
+#include <iostream> // preprocessing directive
 using namespace std;
+#include <iomanip> // directive to use setprecisoin manipulator
 
 int main()
-{
+{	// Declare and defaulting variables
 	double sales = 0.0;
 	double rate = 0.0;
-	char code = '';
+	char code = ' ';
 
+	// Input part of IPO
 	cout << "Sales: ";
-	cin >> sales;
+	cin >> sales; // input for sales
 	cout << "Code (1, 2, 3, or 4): ";
-	cin >> code;
+	cin >> code; // input for code
 
-	if (code == '1' || code == '3')
+	// selection structure
+	if (code == '1' || code == '3') // true path
 		rate = 0.2;
 	else
-		rate = 0.15;
+		rate = 0.15; // false path
 	// end if
 
 	// calculate and display the projected sales amount
-	sales = sales + sales * rate;
-	cout << fixed << setprecision(2);
-	cout << "Projected sales: " << sales << endl;
+	// processing and output part of IPO
+	sales = sales + sales * rate; 
+	cout << fixed << setprecision(2); // setting allowed decimal places to "2"
+	cout << "Projected sales: " << sales << endl; // display sales
 
 	return 0;
 } // end of main function
